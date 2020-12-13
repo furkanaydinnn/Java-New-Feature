b0c6cf59a4644f34580b773472722e1e45910966
furkanaydinnn <furkaanaydin7@gmail.com>
2020-12-13 13:28:40 +0300

    toMap and collectAndThen

--- StreamApiDemo/src/org/demo/streamApi/StreamCollectors.java
+++ StreamApiDemo/src/org/demo/streamApi/StreamCollectors.java
@@ -1,8 +1,11 @@
 package org.demo.streamApi;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.ArrayList;
 
 
@@ -10,13 +13,14 @@ import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toUnmodifiableList;
 import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toSet;
-
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.collectingAndThen;
 
 public class StreamCollectors {
 
 	public static void main(String[] args) {
 		
-		List<String> givenList = Arrays.asList("a", "bb", "ccc", "dd");
+		List<String> givenList = Arrays.asList("a", "bb", "ccc", "dd","bb");
 		
 		/*
 		 * Collectors.toList()
@@ -51,6 +55,24 @@ public class StreamCollectors {
 		 * ToSet collector can be used for collecting all Stream elements into a Set instance.
 		 */
 		Set<String> resultSet = givenList.stream().collect(toSet());
+		resultSet.stream().forEach(str -> System.out.println(str));
+		
+		/*
+		 * Collectors.toMap()
+		 * ToMap collector can be used to collect Stream elements into a Map instance. To do this, we need to provide two functions:
+				keyMapper
+				valueMapper
+			keyMapper will be used for extracting a Map key from a Stream element, and 
+			valueMapper will be used for extracting a value associated with a given key.
+		 */
+		
+		Map<String,Integer> resultMap = givenList.stream().collect(toMap(Function.identity(),String::length,(item, identicalItem) -> item));
+		
+		/*
+		 * Collectors.collectingAndThen()
+		 */
+		
+		givenList.stream().collect(collectingAndThen(toList(), HashSet::new));
 
 	}
 
